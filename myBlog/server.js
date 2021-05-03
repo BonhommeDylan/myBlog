6d47ef03b3f72fc7bae71bed55a37f3eaf1a8304
Dylan Bonhomme <dylan.bonhomme@epitech.eu>
2021-05-03 19:44:15 +0100

    my blog to heroku

---
+++ myBlog/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+var mongoDB = "mongodb+srv://dbUser:s3046971@cluster0.kjgef.mongodb.net/myBlog?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
+
+console.log('Express server listening on port 5000');
